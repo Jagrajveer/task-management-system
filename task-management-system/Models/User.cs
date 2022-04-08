1e506b10f6e50812a4f3b08b385137aeb537e2ce
sahil narwal <sahilnarwal1032@gmail.com>
2022-04-08 03:05:48 -0500

    Models

---
+++ task-management-system/Models/User.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace task_management_system.Models
+{
+    public class User : IdentityUser
+    {
+
+        public DateTime CreatedAt { get; init; }
+
+        public virtual ICollection<ProjectDeveloper> ProjectDevelopers { get; set; }
+        public virtual ICollection<Task> Tasks { get; set; }
+        public virtual ICollection<Notification> Notifications { get; set; }
+
+        public virtual ICollection<Note> Notes { get; set; }
+
+
+        
+        public User()
+        {
+            CreatedAt = DateTime.Now;
+            Notes = new HashSet<Note>();
+            ProjectDevelopers = new HashSet<ProjectDeveloper>();
+            Tasks = new HashSet<Task>();
+            Notifications = new HashSet<Notification>();
+        }
+    }
+
+}
